dda130379fa0a22e1425eb8fa27b4e54be5d1fb4
Joshua Bird <78428514+josh-bird6@users.noreply.github.com>
2026-08-02 10:23:48 -0700

    Create Navigation.R

---
+++ Code/Navigation.R
@@ -0,0 +1,75 @@
+#THE NAVIGATION DOC
+#Variable names and hyperlinks and such from the CDC website
+
+#1999/00
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/1999/DataFiles/SSPFC_A.htm
+#SPFOA - Perfluorooctanoic acid (PFOA)
+#SPFOS - Perfluorooctane sulfonic acid (PFOS)
+#SPFNA - Perfluorononanoic acid (PFNA)
+#SPFHS - Perfluorohexane sulfonic acid (PFHxS)
+
+#2003/04
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2003/DataFiles/L24PFC_C.htm
+#LBXPFOA - Perfluorooctanoic acid (ug/L) (PFOA)
+#LBXPFOS - Perfluorooctane sulfonic acid (ug/L) (PFOS)
+#LBXPFNA - Perfluorononanoic acid (ug/L) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid ug/L) (PFHxS)
+
+#2005/06
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2005/DataFiles/PFC_D.htm
+#LBXPFOA - Perfluorooctanoic acid (ug/L) (PFOA)
+#LBXPFOS - Perfluorooctane sulfonic acid (ug/L) (PFOS)
+#LBXPFNA - Perfluorononanoic acid (ug/L) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid ug/L) (PFHxS)
+
+#2007/08
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2007/DataFiles/PFC_E.htm
+#LBXPFOA - Perfluorooctanoic acid (ug/L) (PFOA)
+#LBXPFOS - Perfluorooctane sulfonic acid (ug/L) (PFOS)
+#LBXPFNA - Perfluorononanoic acid (ug/L) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid ug/L) (PFHxS)
+
+#2009/10
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2009/DataFiles/PFC_F.htm
+#LBXPFOA - Perfluorooctanoic acid (ug/L) (PFOA)
+#LBXPFOS - Perfluorooctane sulfonic acid (ug/L) (PFOS)
+#LBXPFNA - Perfluorononanoic acid (ug/L) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid ug/L) (PFHxS)
+
+#2011/12
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2011/DataFiles/PFC_G.htm
+#LBXPFOA - Perfluorooctanoic acid (ug/L) (PFOA)
+#LBXPFOS - Perfluorooctane sulfonic acid (ug/L) (PFOS)
+#LBXPFNA - Perfluorononanoic acid (ug/L) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid ug/L) (PFHxS)
+
+#2013/14 
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2013/DataFiles/SSPFAS_H.htm
+#NOTEThis is the first year that linear and branched isomers of PFOA and PFOS are disaggregated - they need to be added together to be comparable with previous years 
+#SSNPFOA - Linear perfluorooctanoate (ug/L) (n-PFOA)
+#SSBPFOA - Br. iso of perfluorooctanoate (ug/L) (sB-PFOA)
+#SSNPFOS - Linear perfluorooctane sulfonate (ug/L) (n-PFOS)
+#SSMPFOS - Monomethyl branched iso of PFOS (ug/L) (Sm-PFOS)
+
+#In this year only, these two chemicals are listed separately separate for some daft reason
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2013/DataFiles/PFAS_H.htm
+#LBXPFNA - Perfluorononanoic acid (ug/L) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid (ug/L) (PFHxS)
+
+#2015/16:
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2015/DataFiles/PFAS_I.htm
+#LBXNFOA - n-perfluorooctanoic acid (ng/mL) (n-PFOA)
+#LBXBFOA - Br. perfluorooctanoic acid iso (ng/mL) (sB-PFOA)
+#LBXNFOS - n-perfluorooctane sulfonic acid (ng/mL) (n-PFOS)
+#LBXMFOS - Sm-PFOS (ng/mL) (Sm-PFOS)
+#LBXPFNA - Perfluorononanoic acid (ng/mL) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid (ng/mL) (PFHxS)
+
+#2017/18
+#https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2017/DataFiles/PFAS_J.htm#LBXNFOA
+#LBXNFOA - n-perfluorooctanoic acid (ng/mL) (n-PFOA)
+#LBXBFOA - Br. perfluorooctanoic acid iso (ng/mL) (sB-PFOA)
+#LBXNFOS - n-perfluorooctane sulfonic acid (ng/mL) (n_PFOS)
+#LBXMFOS - Sm-PFOS (ng/mL) (Sm-PFOS)
+#LBXPFNA - Perfluorononanoic acid (ng/mL) (PFNA)
+#LBXPFHS - Perfluorohexane sulfonic acid (ng/mL)  (PFHxS)
